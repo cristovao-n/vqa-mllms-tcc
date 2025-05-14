52f413f2ddf60b0e088093d58e231b23470d50b5
cristovao-n <cristovaon13@gmail.com>
2025-05-13 21:57:26 -0300

    add samples-generator module

--- src/bash/create-sample.sh
+++
@@ -1,7 +0,0 @@
-image_name=$1
-sample_number=$2
-dataset=$3
-
-final_path=stratified-sample/$sample_number/$dataset
-
-mkdir -p $final_path && cp datasets/$dataset/images/$image_name $final_path
--- src/helpers/random.js
+++
@@ -1,6 +0,0 @@
-import seedrandom from "seedrandom";
-
-export function shuffleArray(arr, seed) {
-    const rng = seedrandom(seed);
-    return [...arr].sort(() => 0.5 - rng());
-}
